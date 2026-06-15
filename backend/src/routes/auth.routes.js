d46b438074fa881ca99c0361d78c2b4080b8f8f3
Sebastian Ledesma Arana <U22209664@utp.edu.pe>
2026-06-14 22:56:30 -0500

    ya encontreeee porque no prendia el servidor, me falto agregar la ruta del cambio de contraseña en el auth.routes.js, ya esta solucionado

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { register, login } = require('../controllers/auth.controller')
+const { register, login, changePassword } = require('../controllers/auth.controller')
 const { registerValidator, loginValidator } = require('../middlewares/validators')
 const validate = require('../middlewares/validate.middleware')
-const { register, login, changePassword } = require('../controllers/auth.controller')
 const { verifyToken } = require('../middlewares/auth.middleware')
 
 router.post('/register', registerValidator, validate, register)
 router.post('/login', loginValidator, validate, login)
 router.patch('/change-password', verifyToken, changePassword)
-ñl
+
 module.exports = router
